6b91cc63e3dbc033ae04ce3e9fa7f214dea3fd7f
Jordan Giraldo <jsgiraldo93@gmail.com>
2024-08-28 10:51:01 -0500

    Pruebas unitarias y de integración completas con exito

--- stock/src/test/java/com/emazon/stock/StockApplicationTests.java
+++ stock/src/test/java/com/emazon/stock/StockApplicationTests.java
@@ -1,6 +1,8 @@
 package com.emazon.stock;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
